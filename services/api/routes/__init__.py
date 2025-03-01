4f1245132548d6e314b26a2002a11a34e410b7a8
Hohnik <niklas-hohn@t-online.de>
2025-03-01 19:37:12 +0100

    Add root page

--- services/api/routes/__init__.py
+++ services/api/routes/__init__.py
@@ -1,4 +1,5 @@
+from .root import router as root_router
 from .student import router as student_router
 from .teacher import router as teacher_router
 
-__all__ = ["student_router", "teacher_router"]
+__all__ = ["student_router", "teacher_router", "root_router"]
